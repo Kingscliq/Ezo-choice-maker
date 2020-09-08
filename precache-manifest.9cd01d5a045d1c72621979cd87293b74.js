92b8e26a0c0bb919eb911a6157b07a0649d648c7
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2020-09-08 09:09:33 +0100

    added favicon and title

---
+++ precache-manifest.9cd01d5a045d1c72621979cd87293b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b26ce7b7114afa9fcb453079643b3dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a028f623f210a772251",
+    "url": "/static/css/main.af7a8ca4.chunk.css"
+  },
+  {
+    "revision": "d7093ba5d997719beefd",
+    "url": "/static/js/2.8d8325f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d8325f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a028f623f210a772251",
+    "url": "/static/js/main.95fcc657.chunk.js"
+  },
+  {
+    "revision": "fad63bac8b70cc814d13",
+    "url": "/static/js/runtime-main.2e44b4f6.js"
+  }
+]);
